ec4335d581ec1d79dbf98d03562aa6669f05f406
Himankbareja <himankbareja18638@gmail.com>
2026-03-23 00:23:15 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionopertion.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueinNum = Number(score)
+console.log(typeof valueinNum)
+console.log(valueinNum)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let islogged = "Himank"
+
+let booleanlogged = Boolean(islogged)
+console.log(booleanlogged)
+
+// 1 => true ; 0 => false
+// "" => false
+// "Himank" => true
+// " " => true   (space)
+
+let num = 33
+
+let stringnum = String(num)
+console.log(stringnum)
+console.log(typeof stringnum)
