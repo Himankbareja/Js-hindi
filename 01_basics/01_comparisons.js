b1c512c41ffa3b61af9b9c3ec266f7c2fcf1507a
Himankbareja <himankbareja18638@gmail.com>
2026-03-23 01:30:46 +0000

    comparisons and oprtaions

---
+++ 01_basics/01_comparisons.js
@@ -0,0 +1,21 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)              //  NOT RECOMMENDED TO COMPARE DIFF DATATYPES BUT
+                                    //  IN JAVASCRPT THEY DONT GIVE ERROR
+console.log(null >= 1)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+console.log("2" === 2)
+
+// === -> checks if two datatypes are equal or not
--- 01_basics/01_conversionopertion.js
+++ 01_basics/01_conversionopertion.js
@@ -26,3 +26,40 @@ let num = 33
 let stringnum = String(num)
 console.log(stringnum)
 console.log(typeof stringnum)
+
+//                  OPERTAIONS
+
+
+let value = 3;
+let negval = -value
+console.log(negval)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) // 2 power 3
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = " himank"
+
+let str3  = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)      // true+ will show error
+console.log(+"") 
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let game = 100
+game++
+++game
+console.log(game)
